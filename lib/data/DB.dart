dbee6bc7da611a81bcb7d180dca22192c418c19a
DevLord261 <husseinbayoume@proton.me>
2025-05-22 11:40:59 +0300

    fix errors

--- lib/data/DB.dart
+++ lib/data/DB.dart
@@ -1,8 +1,6 @@
-import 'package:flutterproject/models/Users.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DBContext {
   static final DBContext instance = DBContext.instance;
   late final Database? database;
-  
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutterproject/data/DB.dart';
+
 import 'package:flutterproject/screen/Login.dart';
 
 void main() async {
