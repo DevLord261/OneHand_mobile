c1ab832732277ecc1ceedf43b3b2a0cafae9c705
DevLord261 <husseinbayoume@proton.me>
2025-05-23 17:13:30 +0300

    add hashing

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutterproject/data/DB.dart';
+import 'package:crypto/crypto.dart';
+import 'package:path/path.dart';
 
 class UserService {
   final dbContext = DBContext.instance;
@@ -42,7 +46,7 @@ class UserService {
     required String password,
   }) async {
     final db = await dbContext.database;
-
+    password = hashpassword(password).toString();
     return await db.insert('users', {
       'username': username,
       'email': email,
@@ -50,38 +54,10 @@ class UserService {
     });
   }
 
-  // Update user
-  Future<int> updateUser(
-    int id, {
-    String? firstName,
-    String? lastName,
-    String? username,
-    String? email,
-    String? password,
-  }) async {
-    final db = await dbContext.database;
-
-    final Map<String, dynamic> updates = {};
-    if (firstName != null) updates['first_name'] = firstName;
-    if (lastName != null) updates['last_name'] = lastName;
-    if (username != null) updates['username'] = username;
-    if (email != null) updates['email'] = email;
-    if (password != null) updates['password'] = password;
-
-    if (updates.isEmpty) return 0;
-
-    return await db.update('users', updates, where: 'id = ?', whereArgs: [id]);
-  }
-
-  // Delete user
-  Future<int> deleteUser(int id) async {
-    final db = await dbContext.database;
-    return await db.delete('users', where: 'id = ?', whereArgs: [id]);
-  }
-
   // Login user
   Future<Map<String, dynamic>?> login(String username, String password) async {
     final db = await dbContext.database;
+    password = hashpassword(password).toString();
     final List<Map<String, dynamic>> maps = await db.query(
       'users',
       where: 'username = ? AND password = ?',
@@ -98,9 +74,9 @@ class UserService {
     return null;
   }
 
-  // Get all users
-  Future<List<Map<String, dynamic>>> getAllUsers() async {
-    final db = await dbContext.database;
-    return await db.query('users');
+  Future<String?> hashpassword(String password) async {
+    final bytes = utf8.encode(password);
+    String hashedpassword = sha256.convert(bytes).toString();
+    return hashedpassword;
   }
 }
